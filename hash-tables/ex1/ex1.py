0e3f80aa78c9a766e97804911e044dcca8fd7b96
Daniel Chamorro <dan@dcitllc.com>
2018-10-19 13:36:30 -0400

    ex1 pass

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,6 +1,16 @@
 def get_indices_of_item_weights(weights, limit):
-  pass 
+    weights_dict = {}
+
+    for i in range(len(weights)):
+        weight = weights[i]
+        if (limit - weight) in weights_dict:
+            return (i, weights_dict[limit - weight])
+        else:
+            weights_dict[weight] = i
+
+    return ()
+
 
 if __name__ == '__main__':
-  # You can write code here to test your implementation using the Python repl
-  pass 
+    # You can write code here to test your implementation using the Python repl
+    pass
