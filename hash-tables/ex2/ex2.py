aae03a1c1d4dac4fa583a7317b244ca2fc7e3a89
Daniel Chamorro <dan@dcitllc.com>
2018-10-19 13:45:45 -0400

    ex2 pass

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,6 +1,21 @@
 def reconstruct_trip(tickets):
-  pass 
+    tickets_dict = {}
+    route = [''] * (len(tickets) - 1)
+
+    for ticket in tickets:
+        tickets_dict[ticket[0]] = ticket[1]
+        if ticket[0] is None:
+            route[0] = ticket[1]
+
+    for i in range(1, len(route)):
+        if route[i - 1] in tickets_dict:
+            route[i] = tickets_dict[route[i - 1]]
+        else:
+            return []
+
+    return route
+
 
 if __name__ == '__main__':
-  # You can write code here to test your implementation using the Python repl
-  pass
+    # You can write code here to test your implementation using the Python repl
+    pass
